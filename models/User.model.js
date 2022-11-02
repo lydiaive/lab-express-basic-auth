1d5ab4af1e8ee63972c37c7de3663cb1b00bb0f8
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-11-02 16:27:15 +0100

    Error creating multiple Users

--- models/User.model.js
+++ models/User.model.js
@@ -2,10 +2,10 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  userName: {
+  username: {
     type: String,
-    required: [true, 'Username is required.'],
-    unique: true
+    /* required: [true, 'Username is required.'], */
+    unique: true 
   },
   password: {
     type: String,
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express")
+const router = express.Router();
 const User = require('../models/User.model')
 
 const bcrypt = require('bcryptjs');
@@ -9,21 +10,38 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
+
+// Sign In 
 router.get("/signup", (req, res, next) => {
   res.render("signup");
 });
 
 router.post("/signup", async(req, res, next) => {
-  const dbUser = {userName, password} = req.body
+  const {username, password} = req.body
+  
   const salt = bcrypt.genSaltSync(saltRounds);
   const hash = bcrypt.hashSync(password, salt);
+
+   /* if (username === '' || password === '') {
+    res.render('signup', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }  */
+
   try {
-    const userLogin = await User.create({
-      userName,
+    
+     await User.create({
+      username,
       password: hash
     })
+    
     console.log("You are Signed Up")
-    res.redirect("/");
+    res.redirect("/profile");
   } catch (error) {
     console.log(error)
   }
@@ -31,10 +49,39 @@ router.post("/signup", async(req, res, next) => {
 });
 
 
+// Log In 
 
 router.get("/login", (req, res, next) => {
   res.render("login");
+  
 });
+
+ router.post("/login", async(req, res, next) => {
+  const { userName, password } = req.body;
+  if (userName === '' || password === '') {
+    res.render('login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+  try {
+    const dbUser = await User.findOne({userName})
+    console.log(dbUser)
+
+    if (!dbUser) {
+      res.render('login', { errorMessage: 'Email is not registered. Try with other email.' });
+      return;
+    } else if (bcryptjs.compareSync(password, dbUser.password)) {
+      res.render('profile', { dbUser });
+    } else {
+      res.render('login', { errorMessage: 'Incorrect password.' });
+    }
+    res.redirect("/");
+  } catch (error) {
+    console.log(error)
+  }
+  
+}); 
 
 
 
